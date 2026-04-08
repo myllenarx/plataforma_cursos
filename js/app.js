2efb83f09ec6a66c488ab92b89e7fda059b8a639
myllenarx <myllenarx@gmail.com>
2026-04-08 10:49:51 -0300

    feat: sistema completo com certificados

--- js/app.js
+++ js/app.js
@@ -104,6 +104,7 @@ function cadastrarCurso() {
 
     listarCursos();
     atualizarSelectCursos();
+    atualizarSelectCertificados();
 }
 
 function listarCursos() {
@@ -163,6 +164,7 @@ function cadastrarUsuario() {
 
     listarUsuarios();
     atualizarSelectUsuarios();
+    atualizarSelectCertificados();
 }
 
 function listarUsuarios() {
@@ -238,6 +240,81 @@ function listarMatriculas() {
                 ${usuario?.nome} matriculado em ${curso?.titulo}
                 <br>
                 <small>Data: ${m.data}</small>
+            </li>
+        `;
+    });
+}
+
+class Certificado {
+    constructor(id, idUsuario, idCurso, codigo, data) {
+        this.id = id;
+        this.idUsuario = idUsuario;
+        this.idCurso = idCurso;
+        this.codigo = codigo;
+        this.data = data;
+    }
+}
+
+let certificados = [];
+
+function atualizarSelectCertificados() {
+
+    let selectUser = document.getElementById("selectUsuarioCert");
+    let selectCurso = document.getElementById("selectCursoCert");
+
+    selectUser.innerHTML = `<option value="">Selecione usuário</option>`;
+    selectCurso.innerHTML = `<option value="">Selecione curso</option>`;
+
+    usuarios.forEach(u => {
+        selectUser.innerHTML += `<option value="${u.id}">${u.nome}</option>`;
+    });
+
+    cursos.forEach(c => {
+        selectCurso.innerHTML += `<option value="${c.id}">${c.titulo}</option>`;
+    });
+}
+
+function gerarCertificado() {
+
+    let idUsuario = document.getElementById("selectUsuarioCert").value;
+    let idCurso = document.getElementById("selectCursoCert").value;
+
+    if (idUsuario === "" || idCurso === "") {
+        alert("Selecione usuário e curso!");
+        return;
+    }
+
+    let codigo = "CERT-" + Math.floor(Math.random() * 100000);
+
+    let certificado = new Certificado(
+        Date.now(),
+        idUsuario,
+        idCurso,
+        codigo,
+        new Date().toLocaleDateString()
+    );
+
+    certificados.push(certificado);
+
+    listarCertificados();
+}
+
+function listarCertificados() {
+
+    let lista = document.getElementById("listaCertificados");
+
+    lista.innerHTML = "";
+
+    certificados.forEach(cert => {
+
+        let usuario = usuarios.find(u => u.id == cert.idUsuario);
+        let curso = cursos.find(c => c.id == cert.idCurso);
+
+        lista.innerHTML += `
+            <li class="list-group-item">
+                🎓 ${usuario?.nome} concluiu ${curso?.titulo}
+                <br>
+                <small>Código: ${cert.codigo} | Data: ${cert.data}</small>
             </li>
         `;
     });
